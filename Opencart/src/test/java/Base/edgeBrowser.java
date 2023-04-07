2e4a2d08993e4c75d1901b71eb7e828a9afec2e6
Badri20194841 <20194841@FUE.EDU.EG>
2023-04-07 21:25:23 +0200

    Base Creation

---
+++ Opencart/src/test/java/Base/edgeBrowser.java
@@ -0,0 +1,22 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+public class edgeBrowser {
+    public WebDriver driver;
+    @BeforeTest
+    public void openBrowser(){
+        String edgePath = System.getProperty("user.dir") + "\\src\\main\\resources\\Driver\\msedgedriver.exe";
+        driver = new EdgeDriver();
+        System.setProperty("webdriver.edge.driver",edgePath);
+        driver.get("https://demo.opencart.com/index.php?route=common/home&language=en-gb");
+        driver.manage().window().maximize();
+    }
+    @AfterTest
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+}
---
+++ Opencart/src/test/java/Functions/registration.java
@@ -0,0 +1,11 @@
+package Functions;
+
+import Base.edgeBrowser;
+import org.testng.annotations.Test;
+
+public class registration extends edgeBrowser {
+    @Test
+    public void Start(){
+
+    }
+}
