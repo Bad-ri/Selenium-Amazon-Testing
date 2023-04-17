28f20c5234f4b6677e7d1b22d0edd037d52af881
Badri20194841 <20194841@FUE.EDU.EG>
2023-04-17 16:00:13 +0200

    add json libraire

--- Amazon/src/test/java/Base/Browser.java
+++ Amazon/src/test/java/Base/Browser.java
@@ -10,7 +10,7 @@ import org.testng.annotations.*;
 public class Browser {
     public WebDriver driver;
     protected static Logger logger = LogManager.getLogger();
-
+    int browserCount = 0 ;
     @BeforeClass
     public void openBrowser(){
         String edgePath = System.getProperty("user.dir") + "\\src\\main\\resources\\Driver\\msedgedriver.exe";
@@ -19,20 +19,21 @@ public class Browser {
         //driver.get("https://www.amazon.com/");
         driver.manage().window().maximize();
         //driver.manage().deleteAllCookies();
-        logger.info("************************** Test Execution Started **************************");
-        logger.info("Welcome![browser opens]");
+        browserCount++;
     }
     @AfterSuite
     public void getInfo(ITestContext context) {
+        logger.info("************************** Test Execution Started **************************");
         int total = context.getAllTestMethods().length;
         int passed = context.getPassedTests().size();
         int failed = context.getFailedTests().size();
         int skipped = context.getSkippedTests().size();
-        logger.info("Total number of testcases :" + total);
+        logger.info("Welcome![ "+browserCount+" browsers opened]");
+        logger.info("Total number of testcases : " + total);
         logger.info("Number of testcases Passed : " + passed);
         logger.info("Number of testcases Failed : " + failed);
         logger.info("Number of testcases Skipped  : " + skipped);
-        logger.info("Goodbye![browser close]");
+        logger.info("Goodbye![ "+browserCount+" browsers closed]");
         logger.info("************************** Test Execution Ends **************************");
     }
     @AfterClass
